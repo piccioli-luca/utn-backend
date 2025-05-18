c2cc3bf25f27fbf9b2997cbc7e89796b389a1630
carlarctg <53100513+carlarctg@users.noreply.github.com>
2025-05-18 18:26:19 -0300

    Converts user database to a cat database

--- src/index.ts
+++ src/index.ts
@@ -12,31 +12,35 @@ const connectMongoDB = async () => {
     }
 }
 
-connectMongoDB();
-
-const userSchema = new Schema({
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+const catSchema = new Schema({
+    name: { type: String, required: true },
+    age: { type: Number, required: false, default: null },
+    weight_kg: { type: Number, required: false, default: null },
+    breed: { type: String, required: false, default: null },
+    owner: { type: String, required: false, default: null },
+    color: { type: String, required: true },
 }, {
     versionKey: false,
 })
 
-const User = model("User", userSchema);
+const Cat = model("Cat", catSchema);
 
-interface IUser {
-    username: string;
-    password: string;
-    email: string;
+interface ICat {
+    name: string;
+    color: string;
+    age: number | null;
+    breed: string | null;
+    owner: string | null;
+    weight_kg: number | null;
 }
 
-const getAllUsers = async () => {
+const getAllCats = async () => {
     try {
-        const users = await User.find();
+        const cats = await Cat.find();
         return {
             success: true,
-            data: users,
-            message: "Users retrieved successfully",
+            data: cats,
+            message: "Cats retrieved successfully",
         };
     } catch (error) {
         const e = error as Error;
@@ -47,19 +51,19 @@ const getAllUsers = async () => {
     }
 }
 
-const updateUser = async (id: string, updatedUserData: Partial<IUser>) => {
+const updateCat = async (id: string, updatedCatData: Partial<ICat>) => {
     try {
-        const user = await User.findByIdAndUpdate(id, updatedUserData, { new: true });
-        if (!user || user === null) {
+        const cat = await Cat.findByIdAndUpdate(id, updatedCatData, { new: true });
+        if (!cat || cat === null) {
             return {
                 success: false,
-                message: "User not found",
+                message: "Cat not found",
             }
         }
         return {
             success: true,
-            data: user,
-            message: "User updated successfully",
+            data: cat,
+            message: "Cat updated successfully",
         };
     } catch (error) {
         const e = error as Error;
@@ -70,16 +74,16 @@ const updateUser = async (id: string, updatedUserData: Partial<IUser>) => {
     }
 }
 
-const createUser = async (newUserData: IUser) => {
+const createCat = async (newCatData: ICat) => {
     try {
 
-        const { username, password, email } = newUserData;
-        const newUser = new User({username, password, email});
-        await newUser.save();
+        const { name, age, breed, color, owner, weight_kg } = newCatData;
+        const newCat = new Cat({name, age, breed, color, owner, weight_kg});
+        await newCat.save();
         return {
             success: true,
-            data: newUser,
-            message: "User created successfully",
+            data: newCat,
+            message: "Cat created successfully",
         };
     } catch (error) {
         const e = error as Error;
@@ -90,19 +94,19 @@ const createUser = async (newUserData: IUser) => {
     }
 }
 
-const deleteUser = async (id: string) => {
+const deleteCat = async (id: string) => {
     try {
-        const deletedUser = await User.findByIdAndDelete(id);
-        if (!deletedUser || deletedUser === null) {
+        const deletedCat = await Cat.findByIdAndDelete(id);
+        if (!deletedCat || deletedCat === null) {
             return {
                 success: false,
-                message: "User not found",
+                message: "Cat not found",
             }
         }
         return {
             success: true,
-            data: deletedUser.id,
-            message: "User deleted successfully",
+            data: deletedCat.id,
+            message: "Cat deleted successfully",
         };
     } catch (error) {
         const e = error as Error;
@@ -113,19 +117,19 @@ const deleteUser = async (id: string) => {
     }
 }
 
-const getuserbyID = async (id: string) => {
+const getcatbyID = async (id: string) => {
     try {
-        const user = await User.findById(id);
-        if (!user || user === null) {
+        const cat = await Cat.findById(id);
+        if (!cat || cat === null) {
             return {
                 success: false,
-                message: "User not found",
+                message: "Cat not found",
             }
         }
         return {
             success: true,
-            data: user,
-            message: "User retrieved successfully",
+            data: cat,
+            message: "Cat retrieved successfully",
         };
     } catch (error) {
         const e = error as Error;
@@ -137,13 +141,9 @@ const getuserbyID = async (id: string) => {
 }
 
 const main = async () => {
-    // const user = await createUser({
-    //     username: "testuser",
-    //     password: "testpassword",
-    //     email: "test@gmail.com"
-    // });
-    const users = await getAllUsers();
-    console.log(users);
+    await connectMongoDB();
+    const cats = await getAllCats();
+    console.log(cats);
 }
 
 main();
